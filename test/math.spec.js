87f8d5cd5088abd48abf9f60cd7bd8f99af87bff
Vikas Naithani <132071075+VikasNaithani@users.noreply.github.com>
2025-06-23 11:38:03 +0530

    Update math.spec.js

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,28 +1,10 @@
-//math.spec.js
+import { test, expect } from "@playwright/test";
+import { qase } from "playwright-qase-reporter";
+import { markdownContent } from "./markdownContent";
 
-const math = require('../src/math');
-const { test, expect } = require('@playwright/test');
- 
+test('should display correct homepage title', async ({ page }) => {
+  await page.goto('https://example.com');
 
-test.describe('Math module', () => {
-  test('should correctly add two numbers', () => {
-    expect(math.add(2, 3)).toBe(5);
-  });
-
-  test('should correctly subtract two numbers', () => {
-    expect(math.subtract(5, 3)).toBe(2);
-  });
-  test('[Failed Step Demo]Addition with steps', async () => {
-    await test.step('Start by Adding Number 1', async () => {
-      expect(true).toBe(true);
-    });
-    await test.step('Add another Number 2', async () => {
-      expect(true).toBe(true);
-    });
-    await test.step('Resulting Number should be 3', async () => {
-      expect(math.add(1,2)).toBe(3);
-    });
-    expect(true).toBe(true);
-  });
-
+  // Intentionally failing expectation
+  await expect(page.locator('h1')).toHaveText('Welcome to Example'); // <-- this will fail
 });
